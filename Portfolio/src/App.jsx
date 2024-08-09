0eb7dd97708da952944b8386479db06db803c283
NachoBastianelli <nachobastianelli2003@gmail.com>
2024-08-09 12:20:20 -0300

    First commit - Create a proyect

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[#222] justify-center items-center flex">
+      <h1 className="text-3xl text-white">Hola mundo!</h1>
+    </div>
+  );
+};
+export default App;
