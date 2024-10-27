08ce396ce9e7f98e07517fa09db9b46cedd3ce2d
Nachobastianelli <nachobastianelli2003@gmail.com>
2024-10-26 23:29:29 -0300

    Cambio sqlserver

--- Portfolio/src/mocks/proyectos.jsx
+++ Portfolio/src/mocks/proyectos.jsx
@@ -31,7 +31,7 @@ export const useProyects = () => {
       urlImage: "https://image5.slideserve.com/10806180/ecommerce-api-l.jpg",
       name: t("ecommerceApi.name"),
       description: t("ecommerceApi.description"),
-      link: "",
+      link: "https://github.com/nachobastianelli/EcommerceApiREST",
       tecnologias: [
         {
           logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/2048px-Microsoft_.NET_logo.svg.png",
@@ -47,7 +47,7 @@ export const useProyects = () => {
       urlImage: "https://www.webnode.com/es/blog/files/2019/05/blog2.png",
       name: t("blog.name"),
       description: t("blog.description"),
-      link: "",
+      link: "https://github.com/nachobastianelli/Blog-Front",
 
       tecnologias: [
         {
@@ -63,8 +63,8 @@ export const useProyects = () => {
           name: ".NET",
         },
         {
-          logo: "https://dwglogo.com/wp-content/uploads/2018/03/SQLite_Vector_logo-1024x705.png",
-          name: "SQLite",
+          logo: "https://e7.pngegg.com/pngimages/162/121/png-clipart-microsoft-sql-server-computer-servers-sql-server-management-studio-microsoft-angle-triangle.png",
+          name: "SQL server",
         },
       ],
     },
